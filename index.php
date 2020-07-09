fc7435401239dfd8038bdd73a9aa40865db92709
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-09 19:44:24 -0300

    alter path to css

--- index.php
+++ index.php
@@ -5,13 +5,10 @@
   <title>Document</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
-  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
-
-
-
-  
+ 
 </head>
 <style>
 
