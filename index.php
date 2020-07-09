065649b3ba27eb543781d8509d0a158dd44464df
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-09 20:10:20 -0300

    push theme wordpress

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Document</title>
+  <title><?php geraTitle(); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/fontello.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/assets/css/style.css">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
  
@@ -14,6 +16,19 @@
 
 </style>
 <body>
-
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="content-phone">
+            <div class="item-phone">
+              <i class="icon-phone"></i>
+              <p>(11) 1234-1234</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
 </body>
 </html>
