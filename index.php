6929ec59432c9e4ce78aea3c7be7dde034a2da90
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-09 19:40:09 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/bootstrap.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+  <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
+
+
+
+  
+</head>
+<style>
+
+</style>
+<body>
+
+</body>
+</html>
