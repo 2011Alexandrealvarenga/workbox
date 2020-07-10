c404620a778cda857062de90a0e6b155399a3a83
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-10 07:51:11 -0300

    banner struture

---
+++ functions.php
@@ -0,0 +1,45 @@
+<?php 
+//GERA TITULO DA HEADER
+function geraTitle(){
+
+      bloginfo('name');
+      if (!is_home()) echo ' | ';
+        the_title();
+      }
+
+//REGISTRAR MENU
+function registrar_menu_navegacao(){
+  register_nav_menu('header-menu','main-menu');
+}
+
+add_action('init','registrar_menu_navegacao');
+
+
+
+
+//ARGS DO POST
+function create_post_type(){
+
+  register_post_type('banners',
+    array(
+      'labels' => array(
+      'name' => __('Banners'),
+      'singular_name'=> __('Banners')
+      ),
+      'supports' => array(
+        'title', 'editor','thumbnail'
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'menu_icon' => 'dashicons-images-alt2',
+      'rewrite' => array('slug'=>'banners'),
+    ));
+}
+
+add_action('init','create_post_type');
+
+// DEFINIR AS MINIATURAS DOS POSTS
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(1280,720,'true');//ajusta a imagem para post em rede sociais |true recorta a imagem
+
+?>
--- index.php
+++ index.php
@@ -70,61 +70,64 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <div class="content-title">
-            <h2>SERRALHERIA - INDUSTRIAL</h2>
-            <h4>trabalhamos com</h4>
-            <h3>FERRO e ALUMÍNIO</h3>
-          </div>
-          <div class="content-banner"><!--banner -->
-            <div id="carouselBSWP" class="carousel slide" data-ride="carousel">
-          
-              <div class="carousel-inner">
-              
-                <?php 
-                // args
-                $my_args_banner = array(
-                  'post_type' => 'banners',
-                  'posts_per_page' => 3,
-                );
+          <div class="content-all-banner">
+            <div class="content-title">
+              <h2>SERRALHERIA - INDUSTRIAL</h2>
+              <h4>trabalhamos com</h4>
+              <h3>FERRO e ALUMÍNIO</h3>
+            </div>
+            <div class="content-banner"><!--banner -->
+              <div id="carouselBSWP" class="carousel slide" data-ride="carousel">
+            
+                <div class="carousel-inner">
+                
+                  <?php 
+                  // args
+                  $my_args_banner = array(
+                    'post_type' => 'banners',
+                    'posts_per_page' => 3,
+                  );
 
-                // query
-                $my_query_banner = new WP_Query ( $my_args_banner );
-                ?>
+                  // query
+                  $my_query_banner = new WP_Query ( $my_args_banner );
+                  ?>
 
-                <?php if( $my_query_banner->have_posts()) : 
-                  $banner = $banners[0];
-                  $c = 0;
-                  while( $my_query_banner->have_posts() ) : 
-                  $my_query_banner->the_post(); 
-                ?>
+                  <?php if( $my_query_banner->have_posts()) : 
+                    $banner = $banners[0];
+                    $c = 0;
+                    while( $my_query_banner->have_posts() ) : 
+                    $my_query_banner->the_post(); 
+                  ?>
 
-                  <div class="carousel-item <?php $c++; if($c == 1) { echo ' active'; } ?>">
-                    <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded')) ?>
-                    <div class="carousel-caption d-none d-md-block">
-                      <h5>
-                        <?php the_title(); ?>
-                      </h5>
+                    <div class="carousel-item <?php $c++; if($c == 1) { echo ' active'; } ?>">
+                      <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid img-carousel rounded')) ?>
+                      <div class="carousel-caption d-none d-md-block">
+                        <h5>
+                          <?php the_title(); ?>
+                        </h5>
+                      </div>
                     </div>
-                  </div>
 
-                <?php endwhile; endif; ?>
+                  <?php endwhile; endif; ?>
 
-                <?php wp_reset_query(); ?>
+                  <?php wp_reset_query(); ?>
+                
+                </div>
+
+                <a class="carousel-control-prev" href="#carouselBSWP" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon"></span>
+                  <span class="sr-only">Anterior</span>
+                </a>
+
+                <a class="carousel-control-next" href="#carouselBSWP" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon"></span>
+                  <span class="sr-only">Próximo</span>
+                </a>
               
               </div>
-
-              <a class="carousel-control-prev" href="#carouselBSWP" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-                <span class="sr-only">Anterior</span>
-              </a>
-
-              <a class="carousel-control-next" href="#carouselBSWP" role="button" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-                <span class="sr-only">Próximo</span>
-              </a>
-            
             </div>
           </div>
+            
         </div>
       </div>
     </div>
