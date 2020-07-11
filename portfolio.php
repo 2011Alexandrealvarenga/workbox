686a3b20291aa9f4f9643bfdcfaa5fe3125e635f
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-11 11:44:25 -0300

    content box portfolio

--- portfolio.php
+++ portfolio.php
@@ -16,5 +16,57 @@ Text Domain: THEMA WORKBOX
  ?>
 
 <?php get_header(); ?>
-<p>porfolio</p>
+<section class="portfolio">
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="title">
+          <h2>PORTFOLIO</h2>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div class="client">
+          <h3>nome do cliente</h3>
+        </div>
+      </div>  
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="servico">
+          portoes <br>
+          material <br>
+          endereço <br>
+          contato <br>
+
+        </div>
+      </div>
+      <div class="col">
+        <div class="banner">
+          <img class="img-fluid" src="<?php bloginfo('template_url'); ?>/assets/img/1.jpg">
+        </div>
+      </div>
+    </div>
+        
+    
+  </div>
+  <div class="container">
+    <div class="content">
+      <h3>titulo</h3>
+      <div class="content-box">
+        <div class="content-esquerdo">
+          <p>texto a esquerda</p>
+        </div> 
+        <div class="content-direita">
+          <p>texto a direita</p>
+        </div> 
+      </div>
+      
+
+    </div>
+  </div>
+  
+</section>
 <?php get_footer(); ?>
