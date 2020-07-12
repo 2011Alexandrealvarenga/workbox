6f382045f5eb0d75cbbdbddadbbcdbba92644cec
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-12 11:50:42 -0300

    banner 2

--- index.php
+++ index.php
@@ -16,22 +16,79 @@ Text Domain: THEMA WORKBOX
  ?>
 <?php get_header(); ?>
 <body>
-  
+  <!--
   <section class="banner">
     <div class="container">
       <div class="row">
         <div class="col">
           <div class="content-all-banner">
-            <!--
-            <div class="content-title">
-              <h2>Workbox Serralheria</h2>
-              <p>Trabalhamos com</p>
-              <h3>FERRO e ALUMÍNIO</h3>
-            </div>
-          -->
-            <div class="content-banner"><!--banner -->
+            
+            
+          
+            <div class="content-banner">
               <div id="carouselBSWP" class="carousel slide" data-ride="carousel">
-               
+          
+            <div class="carousel-inner">
+            
+              <?php 
+              // args
+              $my_args_banner = array(
+                'post_type' => 'banners',
+                'posts_per_page' => 3,
+              );
+
+              // query
+              $my_query_banner = new WP_Query ( $my_args_banner );
+              ?>
+
+              <?php if( $my_query_banner->have_posts()) : 
+                $banner = $banners[0];
+                $c = 0;
+                while( $my_query_banner->have_posts() ) : 
+                $my_query_banner->the_post(); 
+              ?>
+
+                <div class="carousel-item <?php $c++; if($c == 1) { echo ' active'; } ?>">
+                  <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded')) ?>
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>
+                      <?php the_title(); ?>
+                    </h5>
+                  </div>
+                </div>
+
+              <?php endwhile; endif; ?>
+
+              <?php wp_reset_query(); ?>
+            
+            </div>
+
+            <a class="carousel-control-prev" href="#carouselBSWP" role="button" data-slide="prev">
+              <span class="carousel-control-prev-icon"></span>
+              <span class="sr-only">Anterior</span>
+            </a>
+
+            <a class="carousel-control-next" href="#carouselBSWP" role="button" data-slide="next">
+              <span class="carousel-control-next-icon"></span>
+              <span class="sr-only">Próximo</span>
+            </a>
+          
+          </div>
+          </div>
+            
+        </div>
+      </div>
+    </div>
+  </section>
+  -->
+  <section class="banner">
+    <div class="container">
+      <div class="row">
+
+       
+          
+            <div id="carouselBSWP" class="carousel slide" data-ride="carousel">
+            
                 <div class="carousel-inner">
                 
                   <?php 
@@ -53,8 +110,7 @@ Text Domain: THEMA WORKBOX
                   ?>
 
                     <div class="carousel-item <?php $c++; if($c == 1) { echo ' active'; } ?>">
-                     
-                      <div class="item">
+                     <div class="item">
                         <div class="titulo">
                           <h2>Workbox Serralheria</h2>
                         </div>
@@ -65,48 +121,39 @@ Text Domain: THEMA WORKBOX
                           <h3>Ferro e Alumínio</h3>
                         </div>
                       </div>
-                    
-                        
-                      
-
-
-                      <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid img-carousel rounded')) ?>
-
+                      <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid rounded')) ?>
                       <div class="carousel-caption d-none d-md-block">
-                        
                         <h5>
                           <?php the_title(); ?>
                         </h5>
-                        
                       </div>
-                      
                     </div>
-                  
+
                   <?php endwhile; endif; ?>
 
                   <?php wp_reset_query(); ?>
-
                 
                 </div>
 
                 <a class="carousel-control-prev" href="#carouselBSWP" role="button" data-slide="prev">
                   <span class="carousel-control-prev-icon"></span>
-                  <span class="sr-only"></span>
+                  <span class="sr-only">Anterior</span>
                 </a>
 
                 <a class="carousel-control-next" href="#carouselBSWP" role="button" data-slide="next">
                   <span class="carousel-control-next-icon"></span>
-                  <span class="sr-only"></span>
+                  <span class="sr-only">Próximo</span>
                 </a>
               
-              </div>
             </div>
-          </div>
-            
-        </div>
+       
+        
       </div>
+
     </div>
+
   </section>
+
   <section class="fazemos">
     <div class="container">
       <div class="row">
