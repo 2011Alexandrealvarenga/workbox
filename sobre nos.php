817f4e7ac20ef205065987c259e956f45b4a6199
Alexandre Bruno <2011brunofernandes@gmail.com>
2020-07-13 21:18:27 -0300

    about alter properties

--- sobre nos.php
+++ sobre nos.php
@@ -20,10 +20,11 @@ Text Domain: THEMA WORKBOX
   <div class="container">
     <div class="row">
       <div class="col">
-        <h2><?php the_title(); ?></h2>
+       
 
            
-            <div class="box-texto">
+            <div class="box-texto"> 
+              <h2><?php the_title(); ?></h2>
 
               <?php if(have_posts()) : while (have_posts()) : the_post();?>
               <div class="texto">
